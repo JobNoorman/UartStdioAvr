73a46f3fde0e303cf88be172c5d27071be84b49c
Job Noorman <job@noorman.info>
2017-08-03 21:48:26 +0200

    Add support for stdin
    Data from the UART is received using interrupts to ensure minimal data
    loss. Currently, there is a 64 byte receive buffer allocated.

--- src/UartStdio.cpp
+++ src/UartStdio.cpp
@@ -3,11 +3,62 @@
 #include <stdio.h>
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/atomic.h>
 
 namespace
 {
 
 FILE UartStdout;
+
+template<size_t Size>
+class Buffer
+{
+public:
+
+    bool write(uint8_t byte) volatile
+    {
+        if (isFull())
+            return false;
+
+        buffer_[writePos_] = byte;
+        writePos_ = nextPos(writePos_);
+        return true;
+    }
+
+    bool read(uint8_t& byte) volatile
+    {
+        if (isEmpty())
+            return false;
+
+        byte = buffer_[readPos_];
+        readPos_ = nextPos(readPos_);
+        return true;
+    }
+
+    bool isEmpty() const volatile
+    {
+        return readPos_ == writePos_;
+    }
+
+    bool isFull() const volatile
+    {
+        return nextPos(writePos_) == readPos_;
+    }
+
+private:
+
+    size_t nextPos(size_t pos) const volatile
+    {
+        return (pos + 1) % Size;
+    }
+
+    uint8_t buffer_[Size];
+    size_t readPos_ = 0;
+    size_t writePos_ = 0;
+};
+
+volatile Buffer<64> RxBuffer;
 
 int UartPutchar(char c, FILE* stream)
 {
@@ -21,6 +72,43 @@ int UartPutchar(char c, FILE* stream)
     return 0;
 }
 
+int UartGetchar(FILE*)
+{
+    if (SREG & (1 << SREG_I))
+    {
+        // If interrupts are enabled, keep trying to read from the buffer until
+        // there is data.
+        uint8_t byte;
+        bool done;
+
+        do
+        {
+            ATOMIC_BLOCK(ATOMIC_FORCEON)
+            {
+                done = RxBuffer.read(byte);
+            }
+        }
+        while (!done);
+
+        return byte;
+    }
+    else
+    {
+        // If interrupts are disabled, first check if there is data in the
+        // buffer. This may happen of interrupts were enabled in the past. If
+        // there is no data in the buffer, wait until there is data in the UART
+        // buffer.
+        uint8_t byte;
+
+        if (RxBuffer.read(byte))
+            return byte;
+
+        while (!(UCSR0A & (1 << RXC0))) {}
+
+        return UDR0;
+    }
+}
+
 }
 
 namespace UartStdio
@@ -28,18 +116,27 @@ namespace UartStdio
 
 void Init()
 {
-    // Baudrate: 115200 @16MHz
-    UBRR0 = 8;
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        // Baudrate: 115200 @16MHz
+        UBRR0 = 8;
 
-    // The default frame settings (8-bit characters, 1 stop bit, no parity bit)
-    // are fine for us
+        // The default frame settings (8-bit characters, 1 stop bit, no parity
+        // bit) are fine for us
 
-    // Enable transmitter
-    UCSR0B = (1 << TXEN0);
+        // Enable transmitter, receiver, and receiver interrupt
+        UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0);
+    }
 
     UartStdout.put = UartPutchar;
-    UartStdout.flags = _FDEV_SETUP_WRITE;
-    stdout = &UartStdout;
+    UartStdout.get = UartGetchar;
+    UartStdout.flags = _FDEV_SETUP_RW;
+    stdout = stdin = &UartStdout;
 }
 
+}
+
+ISR(USART_RX_vect)
+{
+    RxBuffer.write(UDR0);
 }
